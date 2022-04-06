1ac0320d5191bce27af3f3ac113c39de5e6c0c34
Klara Kuret <kuretklara@gmail.com>
2022-04-06 09:36:04 +0200

    Annotates missing regions with existing transcript categories
    By default the script annotates missing regions as they were assigned by segmenting the raw (unfiltered annotation). These categories are intron, CDS, UTR3, UTR5, ncRNA and intergenic.
    If --genic_other flag is used, then missing regions are annotated as "genic_other" and kept as a separate category.

--- MissingAnnotation/ResolveUnannotated.py
+++ MissingAnnotation/ResolveUnannotated.py
@@ -8,17 +8,20 @@ import argparse
 def cli():
     parser = argparse.ArgumentParser(description='Annotate genomic regions that are unnanotated in genomic iCount segmentation.')
     required = parser.add_argument_group('required arguments')
-    required.add_argument('-s', '--segmentation', type=str, required=True,
-                        help='iCount genome level segmentation in GTF format i.e "regions.gtf.gz".')
+    required.add_argument('-fseg', '--filtered_segmentation', type=str, required=True,
+                        help='iCount genome level segmentation in GTF format i.e "regions.gtf.gz", produced from filtered genomic annotation.')
+    required.add_argument('-useg', '--unfiltered_segmentation', type=str, required=True,
+                        help='iCount genome level segmentation in GTF format i.e "regions.gtf.gz", produced from raw genomic annotation.')
     required.add_argument('-a', '--annotation', type=str, required=True,
-                        help='Annotation file from GENCODE or ENSEMBL in GTF format, which was as input for iCount segmentation.')
+                        help='Raw annotation file from GENCODE or ENSEMBL in GTF format.')
     required.add_argument('-fai', '--fasta_index', type=str, required=True,
                         help='Fasta index file generated with samtools.')
     required.add_argument('-o', '--outputdir', type=str, required=True,
                         help='Path to output folder.', default='.')
+    parser.add_argument('-go', '--genic_other', action='store_true')
     args = parser.parse_args()
     print(args)
-    return(args.segmentation, args.annotation, args.fasta_index, args.outputdir)
+    return(args.filtered_segmentation, args.unfiltered_segmentation, args.annotation, args.fasta_index, args.outputdir, args.genic_other)
 
 
 def ReadGtf(segmentation):
@@ -56,38 +59,64 @@ def Fai2Bed(fai):
     bed_chr = pbt.BedTool.from_dataframe(df_chromosomes).sort()
     return(bed_chr)
 
-def run(iCount_segment, gtf_annotation, fai, outputdir):
-    # Read iCount genomic segment and convert it from GTF to BED format.
+def run(filtered_segment, unfiltered_segment, gtf_annotation, fai, outputdir, genic_other=False):
+    # Read filtered iCount genomic segment and convert it from GTF to BED format.
     print('Reading genomic segmentation.')
-    df_segment = ReadGtf(iCount_segment)
-    bed_segment = df_segment.assign(start=df_segment['start']-1, score=0)[['chrom', 'start', 'end', 'name', 'score','strand']]
+    df_segment = ReadGtf(filtered_segment)
+    bed_segment = df_segment.assign(start=df_segment['start']-1, score=0)[['chrom', 'start', 'end', 'feature', 'score','strand']]
     bed_segment = pbt.BedTool.from_dataframe(bed_segment).sort()
+    # Read unfiltered iCount genomic segment and convert it from GTF to BED format.
+    df_unfiltered = ReadGtf(unfiltered_segment)
+    bed_unfiltered = df_unfiltered.assign(start=df_unfiltered['start']-1, score=0)[['chrom', 'start', 'end', 'feature', 'score','strand']]
+    bed_unfiltered = pbt.BedTool.from_dataframe(bed_unfiltered).sort()
+
     # Convert fasta index to BED format - one entry spans one chromosome.
     bed_fai = Fai2Bed(fai)
+
     # Read annotation GTF, keep only gene-level entries and convert it to BED format.
+    print('Getting gene-level annotation...')
     df_annotation = ReadGtf(gtf_annotation)
     df_annotation = df_annotation.loc[df_annotation['feature']=='gene']
     bed_annotation = df_annotation.assign(start=df_annotation['start']-1, score=0)[['chrom', 'start', 'end', 'annotations', 'score', 'strand']]
     bed_annotation = pbt.BedTool.from_dataframe(bed_annotation).sort()
+
     # Find regions that are unannotated in the iCount genome segmentation.
     print('Getting unannotated regions...')
-    bed_missing = bed_fai.subtract(bed_segment, s=True).sort()
+    bed_missing = bed_fai.subtract(bed_segment, s=True,  nonamecheck=True).sort()
     print(f'Found {len(bed_missing)} unannotated genomic regions.')
     # Map gene annotation to the missing regions using bedtools and convert it to GTF format.
-    print('Annotating regions...')
-    bed_missing = bed_missing.map(bed_annotation, s=True, c=4, o='collapse')
-    df_unnanotated = pd.read_csv(bed_missing.fn, sep='\t', header=None, names=['chrom', 'start', 'end', 'name', 'score', 'strand', 'annotations'])
-    # Feature is genic_other.
-    df_unnanotated = df_unnanotated.assign(feature='genic_other',
-                                       start=df_unnanotated['start'] + 1,
-                                       source='.',
-                                       name2='.')
+    print('Annotating regions with gene information...')
+    bed_missing = bed_missing.map(bed_annotation, s=True, c=4, o='collapse', nonamecheck=True)
+    if not genic_other:
+        # Intersect missing regions with unfiltered segment to get transcript region
+        print('Annotating missing regions in iCount segment with transcript regions...')
+        # 1 - Use intersect to split unnanotated regions
+        intersect = bed_missing.intersect(bed_unfiltered, s=True, nonamecheck=True).sort()
+        # 2 - Annotate
+        missingAnnotated = intersect.map(bed_unfiltered, s=True, c=4, o='collapse', nonamecheck=True)
+        df_unnanotated = pd.read_csv(missingAnnotated.fn, sep='\t', header=None, names=['chrom', 'start', 'end', 'name', 'score', 'strand', 'annotations', 'feature'])
+        df_unnanotated = df_unnanotated.assign(start=df_unnanotated['start'] + 1,
+                                               source='.',
+                                               name2='.')
+    else:
+        print('Annotationg missing regions in iCount segment as "genic_other".')
+        # If genic_other flag is enabled, missing regions are annotated as "genic_other"
+        df_unnanotated = pd.read_csv(bed_missing.fn, sep='\t', header=None, names=['chrom', 'start', 'end', 'name', 'score', 'strand', 'annotations'])
+        # Feature is genic_other.
+        df_unnanotated = df_unnanotated.assign(feature='genic_other',
+                                           start=df_unnanotated['start'] + 1,
+                                           source='.',
+                                           name2='.')
     df_unnanotated = df_unnanotated[['chrom', 'source', 'feature', 'start', 'end', 'name', 'strand', 'name2', 'annotations']]
     #Add missing regions to original iCount segment.
-    print('Adding unnanotated regions to iCount segment as "genic_other".')
+    print('Adding annotated missisng regions to iCount segment...')
     df_segment = pd.concat([df_segment, df_unnanotated], ignore_index=True)
     # Sort GTF segment and write it to file
-    outfile = f"{outputdir}/sorted.{iCount_segment.split('/')[-1].replace('.gz', '')}"
+    if genic_other:
+        identifier = 'genic_other'
+    else:
+        identifier = 'annotated'
+    outfile = f"{outputdir}/sorted.{identifier}.{filtered_segment.split('/')[-1].replace('.gz', '')}"
     with tempfile.NamedTemporaryFile(mode='w') as tmpfile:
         df_segment.to_csv(tmpfile.name, index=False, header=False, sep='\t', quoting=csv.QUOTE_NONE)
         cmd = (sort["-t\t", "-k1,1", "-k4,4n", tmpfile.name]) > outfile
@@ -96,8 +125,8 @@ def run(iCount_segment, gtf_annotation, fai, outputdir):
     return 0
 
 def main():
-    (iCount_segment, gtf_annotation, fai, outputdir) = cli()
-    run(iCount_segment, gtf_annotation, fai, outputdir)
+    (filtered_segmentation, unfiltered_segmentation, gtf_annotation, fai, outputdir, genic_other) = cli()
+    run(filtered_segmentation, unfiltered_segmentation, gtf_annotation, fai, outputdir, genic_other)
 
 if __name__ == '__main__':
     main()
