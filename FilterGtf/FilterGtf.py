591c84d1a8763b425e82e6a673bc6787f225e5be
Klara Kuret <kuretklara@gmail.com>
2025-03-12 16:05:07 +0100

    added tests, added print statements for testing, typos

--- FilterGtf/FilterGtf.py
+++ FilterGtf/FilterGtf.py
@@ -33,6 +33,8 @@ def filter_gff(gtf_file, outputdir):
                                     "name2": str,
                                     "annotations": str,
                                     })
+    # Get gene ids from input annotation
+    input_gene_ids = set(input_annotation.loc[input_annotation['feature'] == 'gene', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
     # Filter transcripts by basic tag
     print("Number of entries in input annotation:", len(input_annotation))
     # Check if annotation contains tag "basic"
@@ -48,9 +50,10 @@ def filter_gff(gtf_file, outputdir):
 
         # Check if every gene contains at least one transcript entry
         print("Checking if every gene contains at least one transcript entry: each gene should have a basic set of transcripts.")
-        gene_ids = set(annotation.loc[annotation['feature'] == 'gene', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
         gene_ids_transcripts = set(annotation.loc[annotation['feature'] == 'transcript', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
-        if gene_ids == gene_ids_transcripts:
+        # Print the first five gene ids for visual
+        print("First five gene ids:", list(gene_ids_transcripts)[:5])
+        if input_gene_ids == gene_ids_transcripts:
             print("Every gene contains at least one transcript entry.")
         else:
             print("Some genes do not contain any transcript entry tagged as basic - this indicates that the input gtf may not be correctly annotated or that some entries are missing.")
@@ -70,6 +73,8 @@ def filter_gff(gtf_file, outputdir):
                 df_TSL = annotation.loc[annotation['annotations'].str.contains('transcript_support_level "1|transcript_support_level "2', regex=True), :]
                 # Get gene ids that contain TSL1 or TSL2 transcripts
                 gene_ids_highconf = df_TSL["annotations"].str.split(";", n=1, expand=True)[0].unique().tolist()
+                print("First five gene ids that contain TSL1 or TSL2 transcripts:", gene_ids_highconf[:5])
+                
                 print("Number of genes that contain TSL1 or TSL2 transcripts:", len(gene_ids_highconf))
                 # Keeping only TSL1 and TSL2 entries for genes that contain them, discardig other entries (no TSL information or TSL3-5)
                 print('Filtering out low-confidence transcripts.')
@@ -77,33 +82,26 @@ def filter_gff(gtf_file, outputdir):
                 df_t = annotation.loc[(annotation['feature'] != 'gene') & (annotation['annotations'].str.contains('|'.join(gene_ids_highconf))) , :]
                 # Remove TSL1 and TSL2 entries, this leaves lower-confidence entries
                 df_t = df_t.loc[~df_t['annotations'].str.contains('transcript_support_level "1|transcript_support_level "2', regex=True)]
-
-                # #  Add a check that no entries with TSL1 or TSL2 are kept
-                # if df_t['annotations'].str.contains('transcript_support_level "1|transcript_support_level "2', regex=True).any():
-                #     print('TSL1 / TSL2 are not being properly filtered, this should not happen.')
-                #     print('Returning input annotation as output. Exiting.')
-                #     input_annotation.to_csv(f"{outputdir}/unfiltered.{gtf_file.split('/')[-1]}", header=None, index=None, sep='\t', quoting=csv.QUOTE_NONE)
-                #     return
-                # else:(indent on next line)
+                # Drop non-TSL1 / 2 from annotation
                 annotation.drop(index=df_t.index, inplace=True)
+                # Check that each gene contains at least one transcript entry
+                print("Checking if every gene contains at least one transcript entry after TSL filtering.")
+                # Get gene ids of retained transcripts
+                filtered_gene_ids = set(annotation.loc[annotation['feature'] == 'transcript', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
+                if input_gene_ids == filtered_gene_ids:
+                    print("Every gene contains at least one transcript entry.")
+                else:
+                    print("Some genes do not contain any transcript entry - this indicates that the input gtf may not be correctly annotated or that some entries are missing.")
+                    print('Returning input annotation as output. Exiting.')
+                    input_annotation.to_csv(f"{outputdir}/unfiltered.{gtf_file.split('/')[-1]}", header=None, index=None, sep='\t', quoting=csv.QUOTE_NONE)
+                    return
+
             else:
                 print("No TSL1 or TSL2 flag available.")
                 print("Keeping all transcripts flagged as basic.")
         else:
             print("No transcript_support_level flag available.")
             print("Keeping all transcripts flagged as basic.")
-        # # Check that each gene contains at least one transcript entry
-        # print("Checking if every gene contains at least one transcript entry.")
-        # input_gene_ids = set(input_annotation.loc[input_annotation['feature'] == 'gene', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
-        # filtered_gene_ids = set(annotation.loc[annotation['feature'] == 'gene', 'annotations'].str.split(";", n=1, expand=True)[0].unique().tolist())
-
-        # if input_gene_ids == filtered_gene_ids:
-        #     print("Every gene contains at least one transcript entry.")
-        # else:
-        #     print("Some genes do not contain any transcript entry - this indicates that the input gtf may not be correctly annotated or that some entries are missing.")
-        #     print('Returning input annotation as output. Exiting.')
-        #     input_annotation.to_csv(f"{outputdir}/unfiltered.{gtf_file.split('/')[-1]}", header=None, index=None, sep='\t', quoting=csv.QUOTE_NONE)
-        #     return
         
         print("Number of entries in filtered annotation.", len(annotation))
         print('Saving filtered gtf file.')
